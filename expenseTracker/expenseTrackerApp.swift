d7811440a993c465951d8ad0038780b1dfbb48c4
Sreenitya Yalamanchili <sreenitya1@outlook.com>
2023-07-01 14:29:21 -0400

    Initial Commit

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Purna Yalamanchili on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
